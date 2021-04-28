c777247c8882055e21f5331846a2fa49d71ebaad
SmRanaldi <sm.ranaldi@gmail.com>
2021-04-28 10:44:34 +0200

    Segment-wise whitening

--- conditionEMG.m
+++ conditionEMG.m
@@ -1,8 +1,10 @@
- function [out,outOld]=conditionEMG(signal,language)
+function [out,outOld]=conditionEMG(signal, language, whitenWindow)
 
 % [out]=conditionEMG(signal)
 %
 % Conditioning block for an EMG signal.
+% Consists of a [20, 450] Hz band pass filter, a 50 Hz notch filter
+% and a prewhitening AR filter.
 %
 % INPUTS:
 % 	signal: The EMG signal to be conditioned.
@@ -24,13 +26,39 @@ switch language
         
         signal = signal';
         
-        [out] = whiteningSignal(signal, 13, 150);
+        out = [];
+        k=0;
+        
+        while k+whitenWindow<=length(signal)
+        
+            out = [out; whiteningSignal(signal(k+1:k+whitenWindow), 13, 150)];
+            
+            k = k + whitenWindow;
+            
+        end
+        
+        if k < length(signal)
+            out = [out, whiteningSignal(signal(k+1:end), 13, 150)];
+        end
         
         out=((out./max(out)).*normFactor)';
         
     case 'MATLAB'
         
-        [out] = whitenSignal(signal, 13, 150);
+        out = [];
+        k=0;
+        
+        while k+whitenWindow<=length(signal)
+        
+            out = [out, whitenSignal(signal(k+1:k+whitenWindow), 13, 150)];
+            
+            k = k + whitenWindow;
+            
+        end
+        
+        if k < length(signal)
+            out = [out, whitenSignal(signal(k+1:end), 13, 150)];
+        end
         
         out=((out./max(out)).*normFactor);
         
